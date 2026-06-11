d69dfd4aa55d50696fe165eef3ff3843dcbaa84d
vanshii2441 <vanshikaag0204@gmail.com>
2026-06-11 18:29:24 +0530

    Updated production API URL

--- frontend/src/components/PromptCanvas.jsx
+++ frontend/src/components/PromptCanvas.jsx
@@ -14,7 +14,7 @@ function PromptCanvas({ setResult }) {
     try {
       setLoading(true);
 
-      const response = await fetch("http://127.0.0.1:8000/generate-blueprint", {
+     const response = await fetch("https://project-3e9l.onrender.com/generate-blueprint", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
